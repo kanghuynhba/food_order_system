0ab2ea7bdee12c491284282ee67b42d2b439327e
kanghuynhba <kanghuynhba@gmail.com>
2025-10-03 09:48:33 +0700

    set up Maven

---
+++ src/main/java/com/hbk/food_order_system/App.java
@@ -0,0 +1,13 @@
+package com.hbk.food_order_system;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
